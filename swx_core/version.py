a46b864c680ef96ced9d9b381105540d06ebe6d3
Abdulbasit Aliyu <layinded@gmail.com>
2026-07-29 17:09:29 +0200

    chore: bump version to 2.16.2

--- swx_core/version.py
+++ swx_core/version.py
@@ -31,7 +31,7 @@ class VersionInfo(NamedTuple):
 # Version components
 VERSION_MAJOR = 2
 VERSION_MINOR = 16
-VERSION_PATCH = 1
+VERSION_PATCH = 2
 VERSION_RELEASE = "final"  # alpha, beta, candidate, final
 VERSION_SERIAL = 0
 
